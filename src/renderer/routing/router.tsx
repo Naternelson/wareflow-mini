8e69e2656262434b025ba4510d9be21b22acfa46
Naternelson <7nater.nelson@gmail.com>
2023-10-26 18:49:37 -0600

    Regress to remove contraditory imports

--- src/renderer/routing/router.tsx
+++ src/renderer/routing/router.tsx
@@ -1,19 +1,20 @@
+import React from "react";
 import { createBrowserRouter } from "react-router-dom";
-// import { Home, Landing, ProtectedLayout } from "../views";
+import { Home, Landing, ProtectedLayout } from "../views";
 
 export const router = createBrowserRouter([
-	// {
-	// 	path: "/",
-	// 	element: <ProtectedLayout />,
-	// 	children: [
-	// 		{
-	// 			index: true,
-	// 			element: <Home />,
-	// 		},
-	// 	],
-	// },
-	// {
-	// 	path: "/landing",
-	// 	element: <Landing />,
-	// },
+	{
+		path: "/",
+		element: <ProtectedLayout />,
+		children: [
+			{
+				index: true,
+				element: <Home />,
+			},
+		],
+	},
+	{
+		path: "/landing",
+		element: <Landing />,
+	},
 ]);
--- src/renderer/views/home/ProductLine.tsx
+++ src/renderer/views/home/ProductLine.tsx
@@ -1,12 +1,13 @@
+import React from "react";
 import { Box, Divider, Stack, Typography } from "@mui/material";
-import { OrderStatus, SanitizedOrderItem, SanitizedProduct } from "../../../common/order";
-import { ProgressBar, StatusMenu } from "../../components";
+// import { OrderStatus, SanitizedOrderItem, SanitizedProduct } from "../../../common/order";
+import { ProgressBar } from "../../components";
 import { useState } from "react";
 
-type ProductLineProps = { item: SanitizedOrderItem; product: SanitizedProduct; visible?: boolean };
+type ProductLineProps = /**{ item: SanitizedOrderItem; product: SanitizedProduct; visible?: boolean }*/ {};
 export const ProductLine = (props: ProductLineProps) => {
-	const { item, product } = props;
-	const [value, setValue] = useState(item.status || OrderStatus.QUEUED);
+	// const { item, product } = props;
+	// const [value, setValue] = useState(item.status || OrderStatus.QUEUED);
 	return (
 		<Stack
 			direction={"row"}
@@ -15,18 +16,18 @@ export const ProductLine = (props: ProductLineProps) => {
 			paddingX={"3rem"}
 			paddingY={".5rem"}
 			tabIndex={0}
-			aria-hidden={!props.visible}
+			// aria-hidden={!props.visible}
 			onClick={(e) => {
 				e.currentTarget.focus();
 			}}>
 			<Stack direction={"row"} spacing="1rem" divider={<Divider orientation="vertical" flexItem />}>
-				<ProductName {...product} />
+				{/* <ProductName {...product} /> */}
 
-				<StatusMenu value={value} onChange={setValue} />
-				<OrderQuantity {...item} />
+				{/* <StatusMenu value={value} onChange={setValue} /> */}
+				{/* <OrderQuantity {...item} /> */}
 			</Stack>
 			<Stack direction={"row"} spacing="1rem">
-				{item.quantityOrdered && (
+				{/* {item.quantityOrdered && (
 					<Box minWidth={"10rem"}>
 						<ProgressBar
 							total={item.quantityOrdered}
@@ -36,33 +37,33 @@ export const ProductLine = (props: ProductLineProps) => {
 							color="success"
 						/>
 					</Box>
-				)}
+				)} */}
 			</Stack>
 		</Stack>
 	);
 };
 
-const ProductName = (props: SanitizedProduct) => {
+const ProductName = (props: {}) => {
 	return (
 		<Stack>
 			<Typography variant={"body1"} lineHeight={"1rem"}>
-				{props.name}
+				{/* {props.name} */}
 			</Typography>
 			<Typography variant={"caption"} fontStyle={"italic"} fontWeight={"400"}>
-				ID {props.id}
+				{/* ID {props.id} */}
 			</Typography>
 		</Stack>
 	);
 };
 
-const OrderQuantity = (props: SanitizedOrderItem) => {
+const OrderQuantity = (props: {}) => {
 	return (
 		<Stack direction={"row"} alignItems={"center"} spacing={".5rem"}>
 			<Typography variant="caption" fontWeight={"400"}>
 				Qty
 			</Typography>
 			<Typography variant={"body1"} lineHeight={"1rem"} fontWeight={"600"}>
-				{props.quantityOrdered || "Unknown"}
+				{/* {props.quantityOrdered || "Unknown"} */}
 			</Typography>
 		</Stack>
 	);
